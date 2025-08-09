aeb8ab7e09ca701fa90ca12275ac5084acad6014
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-08-09 00:17:20 -1200

    interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Interfaces");
+        IMovable mycar = new Car();
+        IMovable mybicycle = new Bicycle();
+
+        
+        mycar.Move();
+        mybicycle.Move();
+    }
+}
